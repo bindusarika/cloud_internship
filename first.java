49a1bbbc59dd3d262bebc73640d3529d78c38961
bindusarika <bindusarika333@gmail.com>
2025-03-18 06:18:45 +0000

    added css scripting in the file

--- first.java
+++ first.java
@@ -4,6 +4,18 @@
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
+body {
+  background-color: lightblue;
+}
 
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
 </body>
 </html>
